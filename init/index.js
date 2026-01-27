4a31e2801e505358a7932a1af759ae5cd682acd3
pandayamit725 <amit.pandey@uxdlab.com>
2026-01-27 23:24:27 +0530

    Initial project upload

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing= require("../models/listing.js");
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/traveldot");
+}
+
+main().then(()=>{
+    console.log("mongodb connected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+
+
+
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+}
+
+initDB();
---
+++ models/listing.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema= mongoose.Schema;
+
+
+const listingSchema =new Schema({
+  title:{
+    type:String,
+    required:true,
+  },
+  description:{
+    type:String,
+  },
+  image:{
+    type:String,
+    set:(v)=> v==="" ? "defaultLink":v,
+  },
+  price:{
+    type:Number,
+  },
+  location:{
+    type:String,
+  },
+  country:{
+    type:String,
+  }
+});
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
