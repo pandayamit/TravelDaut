2ea0a3510fd669baef2fb2f1bc0798a5482bb7ff
Amit kumar pandey <you@example.com>
2026-06-15 01:35:48 +0530

    review model added and populated on lisitngs as well

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema,reviewSchema} = require("./schema.js");
+const Review = require("./models/reviews.js")
 
 
 app.set("views", path.join(__dirname, "views"));
@@ -41,7 +42,17 @@ app.get("/", (req, res) => {
 const validateListing= (req,res,next)=>{
     let {error}= listingSchema.validate(req.body);
     if (error){
-        throw new ExpressError(400, error)
+        let errMsg= error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400, errMsg)
+    }
+    next();
+}
+
+const validateReview =(req,res,next)=>{
+    let{error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg= error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400, errMsg);
     }
     next();
 }
@@ -66,7 +77,7 @@ app.get("/listings/new", (req, res) => {
 
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    let listingView = await Listing.findById(id);
+    let listingView = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listingView })
 })
 );
@@ -152,6 +163,21 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     res.redirect(`/listings`);
 })
 );
+
+
+app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req,res)=>{
+    let listing= await Listing.findById(req.params.id)
+    let newReview= new Review(req.body.review);
+    listing.reviews.push(newReview); 
+
+    await newReview.save();
+    await listing.save();
+
+    console.log("new review saved");
+    console.log("Redirecting to:", `/listings/${listing._id}`);
+    res.redirect(`/listings/${listing._id}`);
+    
+}))
 
 // app.get("/testListing", async (req,res) =>{
 //   let sampleListing = new Listing({
--- models/listing.js
+++ models/listing.js
@@ -22,7 +22,13 @@ const listingSchema =new Schema({
   },
   country:{
     type:String,
-  }
+  },
+  reviews:[
+    {
+      type: Schema.Types.ObjectId,
+      ref:"Review",
+    }
+  ]
 });
 
 
